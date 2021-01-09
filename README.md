554fb0b3f766f78d05e58c12b5d8621a9678d8b4
mkDoku <bubecksascha@t-online.de>
2021-01-09 23:39:28 +0100

    Cartesian coordinate and cleaning up
    - Added function to 'animate' Cartesian coordinates
    - Documented functions
    - Added README.md

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Repo for second blog post
+
+## How to use
+
+Change the `Main.hs` file according to the animation you want to compile.
+
+Then compile the animation, e.g. as gif:
+
+```bash
+stack ./src/Main.hs -- render -w 3840 -h 2160 --compile --format gif
+```
+
+## Animations
+
+### Static star
+
+```haskell
+main :: IO ()
+main = reanimate
+  $ docEnv
+  animationStaticStar
+```
+
+![A static star](./images/staticStar.gif)
+
+### Static star (rotated by 90°)
+
+```haskell
+main :: IO ()
+main = reanimate
+  $ docEnv
+  animationStaticStar
+```
+
+![A static star rotated by 90 degrees](./images/staticStar90.gif)
+
+### Demo Cartesian coordinates
+
+```haskell
+main :: IO ()
+main = reanimate
+  $ docEnv
+  animationCartesianCoordinates
+```
+
+![Coordinates of point in Cartesian coordinates](./images/PointCartesian.gif)
--- src/Main.hs
+++ src/Main.hs
@@ -1,37 +1,185 @@
 #!/usr/bin/env stack
 -- stack runghc --package reanimate
+{-# LANGUAGE OverloadedStrings #-}
 module Main ( main ) where
 
 import           Reanimate
 import           Reanimate.Builtin.Documentation
+import           Reanimate.Transition
 
 import           Linear.V2
+
+import qualified Data.Text                       as T
+import           Text.Printf
+
 
 main :: IO ()
 main = reanimate
   $ docEnv
-  $ staticFrame 1
-  $ scale 3.5
-    staticStar
+  animationStaticStar90
 
+-- |
+-- phi
+phi :: Double
+phi = 50
+
+-- |
+-- Aspect ratio
+ratio :: Double
+ratio = 16/9
+
+
+-----------------------------------------------------------------------------
+
+-- |
+-- Static animation to demonstrate Cartesian coordiantes
+animationCartesianCoordinates :: Animation
+animationCartesianCoordinates =
+  -- adjust viewport
+    mapA (withViewBox (-0.5*ratio, -0.5, 1.5*ratio, 1.5))
+  $ chainT parA
+  -- Will only produce one frame. Awesome!
+  -- (1/60) will work for mp4 but not gif
+  $ staticFrame (3/60)
+  <$> [ xAxis
+      , yAxis
+      , point phi
+      , projection X phi
+      , projection Y phi
+      , tic X phi
+      , tic Y phi
+      ]
+
+-- |
+-- Data type to distinguish between
+-- x and y coordinates
+data Dimension = X | Y
+
+-- |
+-- Generate SVG for x axis
+xAxis :: SVG
+xAxis = mkAxis X
+
+-- |
+-- Generate SVG for y axis
+yAxis :: SVG
+yAxis = mkAxis Y
+
+-- |
+-- Helper function to generate
+-- an axis dependent on the 'Dimension'
+mkAxis :: Dimension -> SVG
+mkAxis X = customLine (-2,  0) (5, 0)
+mkAxis Y = customLine (0 , -2) (0, 2)
+
+-- |
+-- Generate modified line
+-- Dot-free notations seems a little bit "weird"
+-- for two arguments
+customLine :: (Double, Double) -> (Double, Double) -> SVG
+customLine = (.) (withStrokeColor "black" . withStrokeWidth 0.004) . mkLine
+
+-- |
+-- Generate SVG of a point with text
+-- of the coordinates (Cartesian)
+point :: Double -> SVG
+point ang = translate x y $ mkGroup
+ [
+      withFillOpacity 1
+    $ withFillColor "black"
+    $ withStrokeColor "white"
+    $ mkCircle 0.01
+  ,
+      translate 0.05 0
+    $ withFillColor "black"
+    $ withFillOpacity 1
+    $ scaleToHeight 0.08
+    $ latex txt
+ ]
+  where (V2 x y) = fromPolarU ang
+        txt      =   T.pack
+                   $ printf "($x$,$y$) = (%1.3f,%1.3f)" x y
+
+-- |
+-- Indicate the projection of (x,y) to either
+-- the x- or y-axis (Cartesian coordinates)
+projection :: Dimension -> Double -> SVG
+projection dim ang =
+  case dim of
+    X -> projectionLine (x, 0) (x, y)
+    _ -> projectionLine (0, y) (x, y)
+    where (V2 x y) = fromPolarU ang
+
+-- |
+-- Modified line for projection
+projectionLine :: (Double, Double) -> (Double, Double) -> SVG
+projectionLine = (.) ( withStrokeDashArray [0.0, 0.01, 0.0]
+                     . withStrokeColor "blue"
+                     . withStrokeWidth 0.002) . mkLine
+
+--
+tic :: Dimension -> Double -> SVG
+tic dim ang =
+  case dim of
+    X -> ticLine (x, -ticklen) (x, ticklen)
+    _ -> ticLine (-ticklen, y) (ticklen, y)
+    where (V2 x y) = fromPolarU ang
+          ticklen  = 0.01
+
+ticLine :: (Double, Double) -> (Double, Double) -> SVG
+ticLine = (.) (   withStrokeColor "black"
+                 . withStrokeWidth 0.004) . mkLine
+
+-----------------------------------------------------------------------------
+
+animationStaticStar90 :: Animation
+animationStaticStar90 =
+    staticFrame (3/60) $ scale 3.5 staticStar90
+
+animationStaticStar :: Animation
+animationStaticStar =
+    staticFrame (3/60) $ scale 3.5 staticStar
+
+staticStar90 :: SVG
+staticStar90 =
+  mkLinePathClosed $ trans coordsInPolar
+    where
+      trans = fmap ( (\(V2 x y) -> (x, y)) . fromPolarU . (+90) )
+
+-- |
+-- Draw symmetric pentagram by using
+-- Cartesian coordinates which are in the
+-- set of the unit circle
 staticStar :: SVG
 staticStar =
   mkLinePathClosed coordsInCartesianU
 
-fromDegrees :: Floating a => a -> a
-fromDegrees deg = deg * pi / 180
+-- |
+-- Take the coordinates for the pentagram in
+-- polar coordinates (0+72*n) with n = 0..5
+-- and transform them into Cartesian coordinates
+coordsInCartesianU :: [(Double, Double)]
+coordsInCartesianU = fmap ( (\(V2 x y) -> (x, y)) . fromPolarU ) coordsInPolar
 
-fromPolar :: Floating a => a -> a -> V2 a
-fromPolar r phi = V2 x y
-  where
-    x = r * sin( fromDegrees phi )
-    y = r * cos( fromDegrees phi )
-
-fromPolarU :: Floating a => a -> V2 a
-fromPolarU = fromPolar 1
-
+-- |
+-- Polar coordinates on the unit circle
 coordsInPolar :: [Double]
 coordsInPolar = [0, 144, 288, 72, 216]
 
-coordsInCartesianU :: [(Double, Double)]
-coordsInCartesianU = fmap ( (\(V2 x y) -> (x, y)) . fromPolarU ) coordsInPolar
+-- |
+-- Transform polar coordinates to Cartesian coordinates (unit circle)
+fromPolarU :: Floating a => a -> V2 a
+fromPolarU = fromPolar 1
+
+-- |
+-- Transform polar coordinates to Cartesian coordinates (arbitrary length)
+fromPolar :: Floating a => a -> a -> V2 a
+fromPolar r ang = V2 x y
+  where
+    x = r * cos( fromDegrees ang )
+    y = r * sin( fromDegrees ang )
+
+-- |
+-- Transform from degrees to radian
+fromDegrees :: Floating a => a -> a
+fromDegrees deg = deg * pi / 180
